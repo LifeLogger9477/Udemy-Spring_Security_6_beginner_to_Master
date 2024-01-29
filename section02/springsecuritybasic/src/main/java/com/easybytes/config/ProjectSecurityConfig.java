93c3e7b6f57d166b0281d07183bcfb0348a1bbf2
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-29 13:33:01 +0900

    section 02: 19. 커스텀 필터 요구 사항에 따른 코드 수정 - 18. 요점 정리에서 lambda 식으로 적용하였으며, 3.2 버전 이상이 적용되도록 수정

--- section02/springsecuritybasic/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ section02/springsecuritybasic/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -2,10 +2,9 @@ package com.easybytes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * author : ms.Lee
@@ -19,10 +18,19 @@ public class ProjectSecurityConfig {
       throws Exception {
 
     http.authorizeHttpRequests(
-        (requests) -> requests.anyRequest().authenticated()
-    );
-    http.formLogin(withDefaults());
-    http.httpBasic(withDefaults());
+            (requests) -> requests
+                .requestMatchers(
+                    "/myAccount",
+                    "/myBalance",
+                    "/myLoans",
+                    "/myCards" ).authenticated()
+                .requestMatchers(
+                    "/notices",
+                    "/contact" ).permitAll()
+        )
+        .formLogin( Customizer.withDefaults() )
+        .httpBasic( Customizer.withDefaults() );
+
     return http.build();
   }
 }
