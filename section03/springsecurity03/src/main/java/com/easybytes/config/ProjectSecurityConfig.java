f9dd79c476c77fce4918b068fd8c92b90b0c1c7b
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-30 12:09:19 +0900

    section 03. 23. InMemoryUserDetailsManager를 사용한 유저 설정 - 접근 방식 1 - 강제 user 생성

--- section03/springsecurity03/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
+++ section03/springsecurity03/src/main/java/com/easybytes/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -32,5 +35,29 @@ public class ProjectSecurityConfig {
         .httpBasic( Customizer.withDefaults() );
 
     return http.build();
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+    /*
+    Approach I where we use withDefaultPasswordEncoder() method
+    while creating the user details
+     */
+
+    // 암호를 plain-text로 사용하려고 withDefaultPasswordEncoder() 사용
+    UserDetails admin = User.withDefaultPasswordEncoder()
+        .username( "admin" )
+        .password( "12345" )
+        .authorities( "admin" )
+        .build();
+
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username( "user" )
+        .password( "12345" )
+        .authorities( "read" )
+        .build();
+
+    return new InMemoryUserDetailsManager( admin, user );
   }
 }
