398f1ec15f85fb81077764f42cc4365f32b65a77
LifeLogger9477 <private.mail.ms.lee@gmail.com>
2024-01-31 12:49:07 +0900

    section 03. 32. 인증을 위한 사용자 정의 테이블 생성 - customer 테이블 생성 + 1개의 데이터 insert

--- section03/springsecurity03/src/main/resources/sql/scripts.sql
+++ section03/springsecurity03/src/main/resources/sql/scripts.sql
@@ -13,7 +13,7 @@ create table users(
 create table authorities (
                              id int not null auto_increment,
                              username varchar(45) not null,
-                             autority varchar(45) not null,
+                             authority varchar(45) not null,
                              primary key (id)
 );
 
@@ -22,5 +22,26 @@ INSERT INTO eazybank.users
 VALUES(1, 'happy', '12345', 0);
 
 INSERT INTO eazybank.authorities
-(id, username, autority)
+(id, username, authority)
 VALUES(1, 'happy', 'write');
+
+create table customer (
+                          id int not null auto_increment,
+                          email varchar(45) not null,
+                          pwd varchar(45) not null,
+                          role varchar(45) not null,
+                          primary key(id)
+);
+
+insert into customer
+(
+    email,
+    pwd,
+    `role`
+)
+values
+(
+    'johndoe@example.com',
+    '54321',
+    'admin'
+);
